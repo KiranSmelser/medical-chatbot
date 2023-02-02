f18a8a71ac743ae1289913713e183d3717d844da
Kiran Smelser <kiran.smelser@gmail.com>
2023-02-01 19:38:23 -0700

    Added multiple scenarios and ran tests

--- main.py
+++ main.py
@@ -1,22 +1,28 @@
 import os
 import openai
 
-API_KEY = 'sk-GhzkOOxKuygy400G9pJCT3BlbkFJXQ45JOTDdJACldfWgE1HH'
+API_KEY = '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'
 os.environ['OPENAI_Key'] = API_KEY
 openai.api_key = os.environ['OPENAI_Key']
 
 
 def main():
+    scenarios = ["a patient with symptoms of the flu that have been developing over the past week during a doctor examination.",
+                 "a patient with chronic high blood pressure during a doctor examination.",
+                 "a patient with a wrist that was broken in a soccer game during a doctor examination.",
+                 "a patient who has been suffering from depression for the past year during an examination with their doctor",
+                 "a patient during their post-surgery follow-up with their doctor after their successful hip surgery"]
+
     keep_prompting = True
     while keep_prompting:
-        user_input = input('What is your question for the patient? Please type \'complete\' when done.')
+        user_input = input('What is your question for the patient? Please type \'complete\' when done.\n')
 
         if user_input == 'complete':
             keep_prompting = False
         else:
             response = openai.Completion.create(
                 model="text-curie-001",
-                prompt=user_input + 'Please respond as if a patient with a migraine during a doctor examination.',
+                prompt=user_input + 'Please respond as if you are ' + scenarios[4],
                 temperature=0.7,
                 max_tokens=256,
                 top_p=1,
