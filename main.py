f3c58a8d4430a0c0e629a4677ca307e74d5a8dd6
Kiran Smelser <kiran.smelser@gmail.com>
2023-02-01 17:36:20 -0700

    Created main.py

---
+++ main.py
@@ -0,0 +1,13 @@
+import openai
+import os
+
+API_KEY = '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'
+os.environ['OPENAI_Key'] = API_KEY
+openai.api_key = os.environ['OPENAI_Key']
+
+def main():
+
+
+
+if __name__ == "__main__":
+    main()
